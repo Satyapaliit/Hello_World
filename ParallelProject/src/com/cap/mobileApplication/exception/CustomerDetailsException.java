9d56f02beae292b2a03fc6f07216839c2becc967
Satyapaliit <35225863+Satyapaliit@users.noreply.github.com>
2019-02-09 00:41:34 +0530

    Update CustomerDetailsException.java

--- ParallelProject/src/com/cap/mobileApplication/exception/CustomerDetailsException.java
+++ ParallelProject/src/com/cap/mobileApplication/exception/CustomerDetailsException.java
@@ -3,13 +3,11 @@ package com.cap.mobileApplication.exception;
 public class CustomerDetailsException extends Exception
 {
 
-	public CustomerDetailsException(String string) {
-		// TODO Auto-generated constructor stub
+	public CustomerDetailsException(String string)
+	{
+		System.out.println(string);
 	}
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 
